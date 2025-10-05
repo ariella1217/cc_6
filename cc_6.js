f538aa3f7a0a68db82be52a6cbad2ce24fb2f0c7
Ariella <ariellayavno@usf.edu>
2025-10-05 18:39:39 -0400

    first commit

---
+++ cc_6.js
@@ -0,0 +1,24 @@
+
+// step 2: Create a base class called Employee with the following properties
+class Employee {
+    constructor(name, department) {
+        this.name = name;
+        this.department = department;
+    }
+
+    describe() {
+        return `${this.name} works in ${this.department} department.`;
+    }
+}
+
+// Step 3: Create a subclass called Manager that extends Employee
+class Manager extends Employee {
+    constructor(name, department, teamSize) {
+        super(name, department);
+        this.teamSize = teamSize;
+    }
+
+    describe() {
+        return `${this.name} manages ${this.teamSize} people in the ${this.department} department.`;
+    }
+}
